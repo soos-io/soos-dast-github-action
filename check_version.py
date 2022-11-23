6a5c6838432f7b1090da2c2bd393470e55490659
SOOS-JAlvarez <jalvarez@soos.io>
2022-11-23 16:45:08 -0300

    final modification on message

--- check_version.py
+++ check_version.py
@@ -3,11 +3,12 @@ import os
 
 
 try:
+    print('-' * 106)
+    print("Checking if your current version is up to date.")
     r = requests.get('https://api.github.com/repos/soos-io/soos-dast-github-action/releases/latest')
     latest_tag = r.json()['tag_name']
     current_tag = os.environ['GITHUB_ACTION_REF'].split('/')[-1]
-    print('-' * 106)
-    print(f"Checking if your current version is up to date. Your current version is: {current_tag}, The latest version is: {latest_tag}")
+    print(f"Your current version is: {current_tag}, The latest version is: {latest_tag}")
     if current_tag == latest_tag:
         print('This action is up to date.')
     else:
