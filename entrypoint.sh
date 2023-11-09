2051ea84028afcd10918f8c06a5df4a26172b57e
SOOS-JAlvarez <jalvarez@soos.io>
2023-11-09 12:30:12 -0300

    PA-11731 Cleanup params of DAST - rework variables.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,151 +6,58 @@ SOOS_APP_VERSION=${GITHUB_ACTION_REF}
 
 cd /zap/
 
-SOOS_CLIENT_ID=$1
-SOOS_API_KEY=$2
-SOOS_PROJECT_NAME=$3
-SOOS_SCAN_MODE=$4
-SOOS_ON_FAILURE=${34}
-SOOS_DEBUG=$5
-SOOS_AJAX_SPIDER=$6
-SOOS_API_BASE_URL=$7
-SOOS_RULES=$8
-SOOS_CONTEXT_FILE=$9
-SOOS_CONTEXT_USER=${10}
-SOOS_FULL_SCAN_MINUTES=${11}
-SOOS_API_SCAN_FORMAT=${12}
-SOOS_LEVEL=${13}
-
-SOOS_BRANCH_NAME=${39}
-SOOS_BRANCH_URI=${14}
-SOOS_BUILD_VERSION=${15}
-SOOS_BUILD_URI=${16}
-SOOS_OPERATING_ENVIRONMENT=${17}
-
-SOOS_ZAP_OPTIONS=${18}
-SOOS_REQUEST_COOKIES=${19}
-SOOS_REQUEST_HEADERS=${20}
-SOOS_OUTPUT_FORMAT=${21}
-
-SOOS_TARGET_URL=${22}
-
-SOOS_REPORT_REQUEST_HEADERS=${23}
-SOOS_BEARER_TOKEN=${24}
-SOOS_AUTH_USERNAME=${25}
-SOOS_AUTH_PASSWORD=${26}
-SOOS_AUTH_LOGIN_URL=${27}
-SOOS_AUTH_USERNAME_FIELD=${28}
-SOOS_AUTH_PASSWORD_FIELD=${29}
-SOOS_AUTH_SUBMIT_FIELD=${30}
-SOOS_AUTH_SUBMIT_ACTION=${31}
-SOOS_OAUTH_TOKEN_URL=${32}
-SOOS_OAUTH_PARAMETERS=${33}
-SOOS_AUTH_SECOND_SUBMIT_FIELD=${35}
-SOOS_AUTH_FORM_TYPE=${36}
-SOOS_AUTH_DELAY_TIME=${37}
-SOOS_DISABLE_RULES=${38}
-
 SOOS_INTEGRATION_NAME="GitHub"
 SOOS_INTEGRATION_TYPE="Plugin"
 
-if [ -z "$SOOS_BRANCH_NAME" ]; then
-    SOOS_BRANCH_NAME=${GITHUB_REF}
-fi
+SOOS_BRANCH_NAME=${SOOS_BRANCH_NAME:-${GITHUB_REF}}
 
-PARAMS="--clientId ${SOOS_CLIENT_ID} --apiKey ${SOOS_API_KEY} --projectName ${SOOS_PROJECT_NAME} --scanMode ${SOOS_SCAN_MODE} --onFailure ${SOOS_ON_FAILURE} --apiURL ${SOOS_API_BASE_URL} --integrationName ${SOOS_INTEGRATION_NAME} --integrationType ${SOOS_INTEGRATION_TYPE} --commitHash ${GITHUB_SHA} --branchName ${SOOS_BRANCH_NAME} --checkoutDir ${GITHUB_WORKSPACE} --appVersion ${SOOS_APP_VERSION}" 
+PARAMS=(
+    "--clientId ${SOOS_CLIENT_ID}"
+    "--apiKey ${SOOS_API_KEY}"
+    "--projectName ${SOOS_PROJECT_NAME}"
+    "--scanMode ${SOOS_SCAN_MODE}"
+    "--onFailure ${SOOS_ON_FAILURE}"
+    "--apiURL ${SOOS_API_BASE_URL}"
+    "--integrationName ${SOOS_INTEGRATION_NAME}"
+    "--integrationType ${SOOS_INTEGRATION_TYPE}"
+    "--commitHash ${GITHUB_SHA}"
+    "--branchName ${SOOS_BRANCH_NAME}"
+    "--checkoutDir ${GITHUB_WORKSPACE}"
+    "--appVersion ${SOOS_APP_VERSION}"
+    ${SOOS_DEBUG:+--debug}
+    ${SOOS_AJAX_SPIDER:+--ajaxSpider}
+    ${SOOS_RULES:+--rules}
+    ${SOOS_CONTEXT_FILE:+--contextFile ${SOOS_CONTEXT_FILE}}
+    ${SOOS_CONTEXT_USER:+--contextUser ${SOOS_CONTEXT_USER}}
+    ${SOOS_FULL_SCAN_MINUTES:+--fullScanMinutes ${SOOS_FULL_SCAN_MINUTES}}
+    ${SOOS_API_SCAN_FORMAT:+--apiScanFormat ${SOOS_API_SCAN_FORMAT}}
+    ${SOOS_LEVEL:+--level ${SOOS_LEVEL}}
+    ${SOOS_BRANCH_URI:+--branchUri ${SOOS_BRANCH_URI}}
+    ${SOOS_BUILD_URI:+--buildUri ${SOOS_BUILD_URI}}
+    ${SOOS_BUILD_VERSION:+--buildVersion ${SOOS_BUILD_VERSION}}
+    ${SOOS_OPERATING_ENVIRONMENT:+--operatingEnvironment ${SOOS_OPERATING_ENVIRONMENT}}
+    ${SOOS_ZAP_OPTIONS:+--zapOptions ${SOOS_ZAP_OPTIONS}}
+    ${SOOS_REQUEST_COOKIES:+--requestCookies ${SOOS_REQUEST_COOKIES}}
+    ${SOOS_REQUEST_HEADERS:+--requestHeaders ${SOOS_REQUEST_HEADERS}}
+    ${SOOS_OUTPUT_FORMAT:+--outputFormat ${SOOS_OUTPUT_FORMAT}}
+    ${SOOS_REPORT_REQUEST_HEADERS:+--reportRequestHeaders ${SOOS_REPORT_REQUEST_HEADERS}}
+    ${SOOS_BEARER_TOKEN:+--bearerToken ${SOOS_BEARER_TOKEN}}
+    ${SOOS_AUTH_USERNAME:+--authUsername ${SOOS_AUTH_USERNAME}}
+    ${SOOS_AUTH_PASSWORD:+--authPassword ${SOOS_AUTH_PASSWORD}}
+    ${SOOS_AUTH_LOGIN_URL:+--authLoginURL ${SOOS_AUTH_LOGIN_URL}}
+    ${SOOS_AUTH_USERNAME_FIELD:+--authUsernameField ${SOOS_AUTH_USERNAME_FIELD}}
+    ${SOOS_AUTH_PASSWORD_FIELD:+--authPasswordField ${SOOS_AUTH_PASSWORD_FIELD}}
+    ${SOOS_AUTH_SUBMIT_FIELD:+--authSubmitField ${SOOS_AUTH_SUBMIT_FIELD}}
+    ${SOOS_AUTH_SUBMIT_ACTION:+--authSubmitAction ${SOOS_AUTH_SUBMIT_ACTION}}
+    ${SOOS_OAUTH_TOKEN_URL:+--oauthTokenUrl ${SOOS_OAUTH_TOKEN_URL}}
+    ${SOOS_OAUTH_PARAMETERS:+--oauthParameters ${SOOS_OAUTH_PARAMETERS}}
+    ${SOOS_AUTH_SECOND_SUBMIT_FIELD:+--authSecondSubmitField ${SOOS_AUTH_SECOND_SUBMIT_FIELD}}
+    ${SOOS_AUTH_FORM_TYPE:+--authFormType ${SOOS_AUTH_FORM_TYPE}}
+    ${SOOS_AUTH_DELAY_TIME:+--authDelayTime ${SOOS_AUTH_DELAY_TIME}}
+    ${SOOS_AUTH_VERIFICATION_URL:+--authVerificationURL ${SOOS_AUTH_VERIFICATION_URL}}
+    ${SOOS_DISABLE_RULES:+--disableRules ${SOOS_DISABLE_RULES}}
+)
 
-if [  "$SOOS_DEBUG" == "true"]; then
-    PARAMS+=" --debug True"
-fi
-if [  "$SOOS_AJAX_SPIDER" == "true" ]; then
-    PARAMS+=" --ajaxSpider True"
-fi
-if [ -n "$SOOS_RULES" ]; then
-    PARAMS+=" --rules ${SOOS_RULES}"
-fi
-if [ -n "$SOOS_CONTEXT_FILE" ]; then
-    PARAMS+=" --contextFile ${SOOS_CONTEXT_FILE}"
-fi
-if [ -n "$SOOS_CONTEXT_USER" ]; then
-    PARAMS+=" --contextUser ${SOOS_CONTEXT_USER}"
-fi
-if [  -n "$SOOS_FULL_SCAN_MINUTES" ]; then
-    PARAMS+=" --fullScanMinutes ${SOOS_FULL_SCAN_MINUTES}"
-fi
-if [  -n "$SOOS_API_SCAN_FORMAT" ]; then
-    PARAMS+=" --apiScanFormat ${SOOS_API_SCAN_FORMAT}"
-fi
-if [  -n "$SOOS_LEVEL" ]; then
-    PARAMS+=" --level ${SOOS_LEVEL}"
-fi
-if [  -n "$SOOS_BRANCH_URI" ]; then
-    PARAMS+=" --branchUri ${SOOS_BRANCH_URI}"
-fi
-if [  -n "$SOOS_BUILD_URI" ]; then
-    PARAMS+=" --buildUri ${SOOS_BUILD_URI}"
-fi
-if [  -n "$SOOS_BUILD_VERSION" ]; then
-    PARAMS+=" --buildVersion ${SOOS_BUILD_VERSION}"
-fi
-if [  -n "$SOOS_OPERATING_ENVIRONMENT" ]; then
-    PARAMS+=" --operatingEnvironment ${SOOS_OPERATING_ENVIRONMENT}"
-fi
-if [  -n "$SOOS_ZAP_OPTIONS" ]; then
-    PARAMS+=" --zapOptions ${SOOS_ZAP_OPTIONS}"
-fi
-if [  -n "$SOOS_REQUEST_COOKIES"  ]; then
-    PARAMS+=" --requestCookies ${SOOS_REQUEST_COOKIES}"
-fi
-if [  -n "$SOOS_REQUEST_HEADERS" ]; then
-    PARAMS+=" --requestHeaders ${SOOS_REQUEST_HEADERS}"
-fi
-if [ -n "$SOOS_OUTPUT_FORMAT" ]; then
-    PARAMS+=" --outputFormat ${SOOS_OUTPUT_FORMAT}"
-fi
-if [ -n "$SOOS_REPORT_REQUEST_HEADERS"]; then
-    PARAMS+=" --reportRequestHeaders  ${SOOS_REPORT_REQUEST_HEADERS}"
-fi
-if [ -n "$SOOS_BEARER_TOKEN" ]; then
-    PARAMS+=" --bearerToken ${SOOS_BEARER_TOKEN}"
-fi
-if [ -n "$SOOS_AUTH_USERNAME" ]; then
-    PARAMS+=" --authUsername ${SOOS_AUTH_USERNAME}"
-fi
-if [ -n "$SOOS_AUTH_PASSWORD" ]; then
-    PARAMS+=" --authPassword ${SOOS_AUTH_PASSWORD}"
-fi
-if [ -n "$SOOS_AUTH_LOGIN_URL" ]; then
-    PARAMS+=" --authLoginURL ${SOOS_AUTH_LOGIN_URL}"
-fi
-if [ -n "$SOOS_AUTH_USERNAME_FIELD" ]; then
-    PARAMS+=" --authUsernameField ${SOOS_AUTH_USERNAME_FIELD}"
-fi
-if [ -n "$SOOS_AUTH_PASSWORD_FIELD" ]; then
-    PARAMS+=" --authPasswordField ${SOOS_AUTH_PASSWORD_FIELD}"
-fi
-if [ -n "$SOOS_AUTH_SUBMIT_FIELD" ]; then
-    PARAMS+=" --authSubmitField ${SOOS_AUTH_SUBMIT_FIELD}"
-fi
-if [ -n "$SOOS_AUTH_SUBMIT_ACTION" ]; then
-    PARAMS+=" --authSubmitAction ${SOOS_AUTH_SUBMIT_ACTION}"
-fi
-if [ -n "$SOOS_OAUTH_TOKEN_URL" ]; then
-    PARAMS+=" --oauthTokenUrl ${SOOS_OAUTH_TOKEN_URL}"
-fi
-if [ -n "$SOOS_OAUTH_PARAMETERS" ]; then
-    PARAMS+=" --oauthParameters ${SOOS_OAUTH_PARAMETERS}"
-fi
-if [  -n "$SOOS_AUTH_SECOND_SUBMIT_FIELD" ]; then
-    PARAMS+=" --authSecondSubmitField ${SOOS_AUTH_SECOND_SUBMIT_FIELD}"
-fi
-if [  -n "$SOOS_AUTH_FORM_TYPE" ]; then
-    PARAMS+=" --authFormType ${SOOS_AUTH_FORM_TYPE}"
-fi
-if [  -n "$SOOS_AUTH_DELAY_TIME" ]; then
-    PARAMS+=" --authDelayTime ${SOOS_AUTH_DELAY_TIME}"
-fi
-if [ -n "$SOOS_DISABLE_RULES" ]; then
-    PARAMS+=" --disableRules ${SOOS_DISABLE_RULES}"
-fi
+PARAMS_STRING="${PARAMS[@]}"
 
-python3 main.py ${SOOS_TARGET_URL} ${PARAMS}
+python3 main.py "${SOOS_TARGET_URL}" ${PARAMS_STRING}
