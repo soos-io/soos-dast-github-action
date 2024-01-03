ea71ed3228863f21d20e2f914affd5d666520d2d
SOOS-JAlvarez <jalvarez@soos.io>
2024-01-03 09:44:23 -0300

    contemplate branch names

--- check_version.py
+++ check_version.py
@@ -1,26 +1,36 @@
 import requests
 import os
 
+
+def is_version_number(string):
+    """Check if a string is a semantic version number."""
+    return re.match(r'^v?\d+\.\d+\.\d+$', string) is not None
+
+
 try:
     print('-' * 106)
     print("Checking if your current version is up to date.")
     r = requests.get(
         'https://api.github.com/repos/soos-io/soos-dast-github-action/releases/latest')
     latest_tag = r.json()['tag_name']
+    # Convert to integer for comparison
     latest_tag_major = int(latest_tag.split('.')[0].lstrip('v'))
     current_tag = os.environ['GITHUB_ACTION_REF'].split('/')[-1]
-    current_tag_major = int(current_tag.split('.')[0].lstrip('v'))
 
     print(
         f"Your current version is: {current_tag}, The latest version is: {latest_tag}")
 
-    if current_tag == latest_tag:
-        print('This action is up to date.')
-    elif current_tag_major < latest_tag_major:
-        raise Exception(
-            f'This action is outdated. Please update to a version not older than @v{latest_tag_major}.')
+    if is_version_number(current_tag):
+        current_tag_major = int(current_tag.split('.')[0].lstrip('v'))
+        if current_tag == latest_tag:
+            print('This action is up to date.')
+        elif current_tag_major < latest_tag_major:
+            raise Exception(
+                f'This action is outdated. Please update to a version not older than @v{latest_tag_major}.')
+        else:
+            print('Your version is up to date with the latest major release.')
     else:
-        print('Your version is up to date with the latest major release.')
+        print('Current tag does not represent a version number, likely a branch name. Skipping version check.')
 
 except requests.exceptions.RequestException as e:
     print('Network error while checking for updates. Please check manually.')
