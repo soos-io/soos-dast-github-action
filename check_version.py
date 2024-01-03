cb0d5d0a4dbbda2d6833f33adacb186131240fd1
SOOS-JAlvarez <jalvarez@soos.io>
2024-01-03 11:28:33 -0300

    c

--- check_version.py
+++ check_version.py
@@ -12,23 +12,31 @@ def is_version_number(string):
 try:
     print('-' * 106)
     print("Checking if your current version is up to date.")
-    r = requests.get(
+    latest_release = requests.get(
         'https://api.github.com/repos/soos-io/soos-dast-github-action/releases/latest')
-    latest_tag = r.json()['tag_name']
+    action_tags = requests.get(
+        'https://api.github.com/repos/soos-io/soos-dast-github-action/tags')
+
+    starter_version = next(
+        (tag for tag in action_tags.json() if tag['name'] == 'starter_version'), None)
+
+    latest_tag = latest_release.json()['tag_name']
     latest_tag_major = int(latest_tag.split('.')[0].lstrip('v'))
     current_tag = os.environ['GITHUB_ACTION_REF'].split('/')[-1]
 
     print(
         f"Your current version is: {current_tag}, The latest version is: {latest_tag}")
 
-    if is_version_number(current_tag):
+    if current_tag == starter_version['commit']['sha']:
+        print('This action matches the starter version. Exiting...')
+        sys.exit(1)
+    elif is_version_number(current_tag):
         current_tag_major = int(current_tag.split('.')[0].lstrip('v'))
         if current_tag == latest_tag:
             print('This action is up to date.')
         elif current_tag_major < latest_tag_major:
             print(
                 f'This action is outdated. Please update to a version not older than @v{latest_tag_major}.')
-            sys.exit(1)
         else:
             print('Your version is up to date with the latest major release.')
     else:
