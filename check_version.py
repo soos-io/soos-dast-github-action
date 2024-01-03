29e3ae67932247ea51135925b4d0c6774624e54a
SOOS-JAlvarez <jalvarez@soos.io>
2024-01-03 09:40:56 -0300

    review

--- check_version.py
+++ check_version.py
@@ -1,24 +1,30 @@
 import requests
 import os
 
-
 try:
     print('-' * 106)
     print("Checking if your current version is up to date.")
     r = requests.get(
         'https://api.github.com/repos/soos-io/soos-dast-github-action/releases/latest')
     latest_tag = r.json()['tag_name']
-    latest_tag_major = latest_tag.split('.')[0]
+    latest_tag_major = int(latest_tag.split('.')[0].lstrip('v'))
     current_tag = os.environ['GITHUB_ACTION_REF'].split('/')[-1]
-    current_tag_major = current_tag.split('.')[0]
+    current_tag_major = int(current_tag.split('.')[0].lstrip('v'))
+
     print(
         f"Your current version is: {current_tag}, The latest version is: {latest_tag}")
+
     if current_tag == latest_tag:
         print('This action is up to date.')
-    elif current_tag_major != latest_tag_major:
+    elif current_tag_major < latest_tag_major:
         raise Exception(
-            'This action is outdated. Please update to the latest version: @v' + latest_tag_major)
-except Exception as error:
-    print('Failed to check for updates. Please check manually.')
+            f'This action is outdated. Please update to a version not older than @v{latest_tag_major}.')
+    else:
+        print('Your version is up to date with the latest major release.')
+
+except requests.exceptions.RequestException as e:
+    print('Network error while checking for updates. Please check manually.')
+except KeyError:
+    print('Error parsing JSON response. Please check manually.')
 
 print('-' * 106)
