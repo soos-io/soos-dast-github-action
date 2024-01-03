59559b55d42b6310cf034eb713111c750fbd2708
SOOS-JAlvarez <jalvarez@soos.io>
2024-01-03 12:15:37 -0300

    review

--- check_version.py
+++ check_version.py
@@ -1,50 +1,36 @@
 import requests
 import os
-import re
 import sys
-
-
-def is_version_number(string):
-    """Check if a string is a semantic version number."""
-    return re.match(r'^v?\d+\.\d+\.\d+$', string) is not None
-
 
 try:
     print('-' * 106)
     print("Checking if your current version is up to date.")
-    latest_release = requests.get(
+
+    response = requests.get(
         'https://api.github.com/repos/soos-io/soos-dast-github-action/releases/latest')
-    action_tags = requests.get(
-        'https://api.github.com/repos/soos-io/soos-dast-github-action/tags')
 
-    starter_version = next(
-        (tag for tag in action_tags.json() if tag['name'] == 'starter_version'), None)
-
-    latest_tag = latest_release.json()['tag_name']
-    latest_tag_major = int(latest_tag.split('.')[0].lstrip('v'))
+    latest_tag = response.json()['tag_name']
     current_tag = os.environ['GITHUB_ACTION_REF'].split('/')[-1]
 
-    print(
-        f"Your current version is: {current_tag}, The latest version is: {latest_tag}")
+    latest_tag_major = latest_tag.split('.')[0]
 
-    if current_tag == starter_version['commit']['sha']:
-        print('This action matches the starter version. Exiting...')
+    print(
+        f"Your current version is: {current_tag}, The latest version is: {latest_tag_major}")
+
+    if not current_tag.startswith(latest_tag_major):
+        print(
+            f"This action is outdated. Please update to the latest version: {latest_tag_major}")
         sys.exit(1)
-    elif is_version_number(current_tag):
-        current_tag_major = int(current_tag.split('.')[0].lstrip('v'))
-        if current_tag == latest_tag:
-            print('This action is up to date.')
-        elif current_tag_major < latest_tag_major:
-            print(
-                f'This action is outdated. Please update to a version not older than @v{latest_tag_major}.')
-        else:
-            print('Your version is up to date with the latest major release.')
+
+    if current_tag == latest_tag:
+        print('This action is up to date.')
     else:
-        print('Current tag does not represent a version number, likely a branch name. Skipping version check.')
+        print(
+            f"This action is outdated. Please update to the latest version: {latest_tag}")
 
 except requests.exceptions.RequestException as e:
-    print('Network error while checking for updates. Please check manually.')
-except KeyError:
-    print('Error parsing JSON response. Please check manually.')
+    print(f'Failed to check for updates due to a network error: {e}')
+except Exception as error:
+    print(f'An error occurred: {error}')
 
 print('-' * 106)
