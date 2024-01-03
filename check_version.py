04712eb99f4207fdd109472cbbef3a7429f08996
SOOS-JAlvarez <jalvarez@soos.io>
2024-01-03 09:35:21 -0300

    PA-12485 Force update to latest major

--- check_version.py
+++ check_version.py
@@ -5,14 +5,19 @@ import os
 try:
     print('-' * 106)
     print("Checking if your current version is up to date.")
-    r = requests.get('https://api.github.com/repos/soos-io/soos-dast-github-action/releases/latest')
+    r = requests.get(
+        'https://api.github.com/repos/soos-io/soos-dast-github-action/releases/latest')
     latest_tag = r.json()['tag_name']
+    latest_tag_major = latest_tag.split('.')[0]
     current_tag = os.environ['GITHUB_ACTION_REF'].split('/')[-1]
-    print(f"Your current version is: {current_tag}, The latest version is: {latest_tag}")
+    current_tag_major = current_tag.split('.')[0]
+    print(
+        f"Your current version is: {current_tag}, The latest version is: {latest_tag}")
     if current_tag == latest_tag:
         print('This action is up to date.')
-    else:
-        print('This action is outdated. Please update to the latest version: ' + latest_tag)
+    elif current_tag_major != latest_tag_major:
+        raise Exception(
+            'This action is outdated. Please update to the latest version: @v' + latest_tag_major)
 except Exception as error:
     print('Failed to check for updates. Please check manually.')
 
