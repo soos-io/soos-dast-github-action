ffc72d7c4780f28e755c755a727c9b824e7c57ac
SOOS-JAlvarez <jalvarez@soos.io>
2024-01-17 14:58:29 -0300

    alpha

--- check_version.py
+++ check_version.py
@@ -1,36 +1,21 @@
 import requests
 import os
-import sys
+
 
 try:
     print('-' * 106)
     print("Checking if your current version is up to date.")
-
-    response = requests.get(
+    r = requests.get(
         'https://api.github.com/repos/soos-io/soos-dast-github-action/releases/latest')
-
-    latest_tag = response.json()['tag_name']
+    latest_tag = r.json()['tag_name']
     current_tag = os.environ['GITHUB_ACTION_REF'].split('/')[-1]
-
-    latest_tag_major = latest_tag.split('.')[0]
-
     print(
-        f"Your current version is: {current_tag}, The latest version is: {latest_tag_major}")
-
-    if not current_tag.startswith(latest_tag_major):
-        print(
-            f"This action is outdated. Please update to the latest version: {latest_tag_major}")
-        sys.exit(1)
-
+        f"Your current version is: {current_tag}, The latest version is: {latest_tag}")
     if current_tag == latest_tag:
         print('This action is up to date.')
     else:
-        print(
-            f"This action is outdated. Please update to the latest version: {latest_tag}")
-
-except requests.exceptions.RequestException as e:
-    print(f'Failed to check for updates due to a network error: {e}')
+        print('This action is outdated. Please update to the latest version: ' + latest_tag)
 except Exception as error:
-    print(f'An error occurred: {error}')
+    print('Failed to check for updates. Please check manually.')
 
 print('-' * 106)
