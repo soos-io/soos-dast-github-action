743e1608d240eb8845d636b11f7260db3d001b18
SOOS-GSteen <gsteen@soos.io>
2024-07-25 10:54:17 -0400

    c

--- check_version.py
+++ check_version.py
@@ -6,27 +6,23 @@ try:
     print('-' * 106)
     print("Checking if your current version is up to date.")
 
-    response = requests.get(
-        'https://api.github.com/repos/soos-io/soos-dast-github-action/releases/latest')
+    response = requests.get('https://api.github.com/repos/soos-io/soos-dast-github-action/releases/latest')
 
     latest_tag = response.json()['tag_name']
     current_tag = os.environ['GITHUB_ACTION_REF'].split('/')[-1]
 
     latest_tag_major = latest_tag.split('.')[0]
 
-    print(
-        f"Your current version is: {current_tag}, The latest version is: {latest_tag_major}")
+    print(f"Your current version is: {current_tag}, The latest version is: {latest_tag_major}")
 
     if not current_tag.startswith(latest_tag_major):
-        print(
-            f"This action is outdated. Please update to the latest version: {latest_tag_major}")
+        print(f"This action is outdated. Please update to the latest version: {latest_tag_major}")
         sys.exit(1)
 
     if current_tag == latest_tag:
-        print('This action is up to date.')
-    else:
-        print(
-            f"This action is outdated. Please update to the latest version: {latest_tag}")
+        print(f"It is recommended to use the major version tag, {latest_tag_major} when referencing this action.")
+    else if not current_tag == latest_tag_major:
+        print(f"This action is out of date. It is recommended to use the major version tag, {latest_tag_major}.")
 
 except requests.exceptions.RequestException as e:
     print(f'Failed to check for updates due to a network error: {e}')
